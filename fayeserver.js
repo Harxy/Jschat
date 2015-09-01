5cf53902d844f498a166ae643583ba5b44cbb6d1
Paul Harker <harkerp@gmail.com>
2015-09-01 12:41:39 +0100

    heroku fixes

--- fayeserver.js
+++ fayeserver.js
@@ -1,18 +1,18 @@
 var http = require('http'),
-    static = require('node-static');
+    static = require('node-static'),
     Faye = require('faye');
 
     var port = process.env.PORT || 8001;
 
+var bayeux = new Faye.NodeAdapter({mount: '/faye', timeout: 5 });
+
 var file = new(static.Server)('.');
 
-var server = http.createServer(function (request, response) {
-    request.addListener('end', function () {
-        fileServer.serve(request, response);
+var server = http.createServer(function(request, response) {
+    request.addListener('end', function() {
+        file.serve(request, response);
     });
   });
-
-var bayeux = new Faye.NodeAdapter({mount: '/faye', timeout: 5 });
 
 bayeux.attach(server);
 server.listen(port);
