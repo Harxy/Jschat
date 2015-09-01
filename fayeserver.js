ebce9867073a5b4ffdbe74033aea10e0d3e4e43a
Paul Harker <harkerp@gmail.com>
2015-09-01 13:30:15 +0100

    could work...

--- fayeserver.js
+++ fayeserver.js
@@ -1,18 +1,20 @@
-var http = require('http'),
-    static = require('node-static'),
-    Faye = require('faye');
+var    statix = require('node-static'),
+       http = require('http'),
+       Faye = require('faye');
 
-    var port = process.env.PORT || 8001;
+
 
 var bayeux = new Faye.NodeAdapter({mount: '/faye', timeout: 5 });
 
-var file = new(static.Server)('.');
+var file = new(statix.Server)('.');
 
 var server = http.createServer(function(request, response) {
+    console.log('accessing');
     request.addListener('end', function() {
         file.serve(request, response);
-    });
+    }).resume();
   });
-
 bayeux.attach(server);
+var port = process.env.PORT || 8001;
 server.listen(port);
+console.log('listening on: ' + port);
