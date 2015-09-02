fc72b4bc0be3327504d84b72f5766ac7f80f386f
johndarv <johndarv@hotmail.com>
2015-09-02 09:43:13 +0100

    Removing unused files and removing the node-static stuff from the server.

--- fayeclient2.js
+++
@@ -1,8 +0,0 @@
-var Faye = require('faye');
-
-var client = new Faye.Client('http://localhost:8001/faye');
-
-client.subscribe('/messages', function(message) {
-  console.log('got a message: ' + message.text);
-
-})
--- fayeserver.js
+++ fayeserver.js
@@ -1,19 +1,13 @@
-var    statix = require('node-static'),
-       http = require('http'),
+var    http = require('http'),
        Faye = require('faye');
 
+var server = http.createServer();
 
+var bayeux = new Faye.NodeAdapter({ mount: '/faye', timeout: 5 });
 
-var bayeux = new Faye.NodeAdapter({mount: '/faye', timeout: 5 });
-
-var file = new(statix.Server)('.');
-
-var server = http.createServer(function(request, response) {
-    request.addListener('end', function() {
-        file.serve(request, response);
-    }).resume();
-  });
 bayeux.attach(server);
+
 var port = process.env.PORT || 8001;
 server.listen(port);
+
 console.log('listening on: ' + port);
