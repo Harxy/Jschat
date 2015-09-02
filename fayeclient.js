fef1c8495b99c09626728f4690cbb1835faae653
Oli Farago <oli.farago@m7re.co.uk>
2015-09-02 14:56:46 +0100

    Removed unused files

--- fayeclient.js
+++
@@ -1,9 +0,0 @@
-var Faye = require('faye');
-
-var client = new Faye.Client('http://localhost:8001/faye');
-
-var pub = client.publish('/messages', {
-  text: 'Hello World'
-});
-
-pub.callback(process.exit);
