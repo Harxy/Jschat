57c1c2ea41b5a959563b8ed81fa9bb9e02450b7d
steve.brazier <steve.brazier@just-eat.com>
2015-09-03 10:56:33 +0100

    filter all html except marquees

--- extensions/script-filter.js
+++ extensions/script-filter.js
@@ -1,36 +1,17 @@
-String.prototype.filterOutScriptTags = function () {
-    var i = 0, scriptTagLevel = 0;
-    var result = "";
-    var str = this.toString();
-    while (i < str.length) {
-        // if we're at the start of a script tag, increase the scriptTagLevel
-        if (str.length - i >= 7 && str.substring(i, i + 7) == "\<script") {
-            scriptTagLevel++;
-            i = i + 7;
-            // if we're at the end of a script tag, decrease the scriptTagLevel (don't let it go below 0)
-        } else if (str.length - i >= 9 && str.substring(i, i + 9) == "\</script\>") {
-            if (scriptTagLevel > 0) {
-                scriptTagLevel--;
-            }
+var Entities = require('html-entities').AllHtmlEntities;
+entities = new Entities();
 
-            i = i + 9;
-            // else, use the scriptTagLevel to determine whether to include the text or not
-        } else {
-            if (scriptTagLevel == 0) {
-                result += str[i];
-            }
-
-            i++;
-        }
-    }
-
-    return result;
+var putTheFunBackIn = function(text) {
+    return text
+        .replace(/&lt;marquee&gt;/ig, "<marquee>")
+        .replace(/&lt;\/marquee&gt;/ig, "</marquee>");
 };
 
 var ScriptFilterExtension = {
     incoming: function (message, callback) {
         if (message.data && message.data.text) {
-            message.data.text = message.data.text.filterOutScriptTags();
+            message.data.text = entities.encode(message.data.text);
+            message.data.text = putTheFunBackIn(message.data.text);
         }
         callback(message);
     }
