9d1cb1226287bc28c1fa7e02b2c9816091c4aa06
Oli Farago <oli.farago@m7re.co.uk>
2015-09-03 12:53:54 +0100

    Persist 30 messages in a room

--- fayeserver.js
+++ fayeserver.js
@@ -3,7 +3,10 @@ var    statix = require('node-static'),
        Faye = require('faye'),
        request = require("request"),
        diceRollExtension = require("./extensions/dice-roll"),
-       gifMeExtension = require("./extensions/gif-me");
+       gifMeExtension = require("./extensions/gif-me"),
+       dataStore = require('node-persist');
+
+dataStore.initSync();
 
 var bayeux = new Faye.NodeAdapter({mount: '/faye', timeout: 5 });
 
@@ -11,10 +14,21 @@ var file = new(statix.Server)('.');
 
 var server = http.createServer(function(request, response) {
     request.addListener('end', function() {
-      if (request.url.indexOf('/rooms/') === 0)
+        if (request.url.indexOf('/rooms/') === 0) {
             file.serveFile('/index.html', 200, {}, request, response);
-        else
+        } else if (request.url.indexOf('/history/') === 0) {
+            var room = request.url.slice(request.url.lastIndexOf('/') + 1);
+            if (!room)
+                room = 'undefined';
+
+            var dataStoreId = 'messages.' + room;
+            var messages = dataStore.getItem(dataStoreId);
+            response.writeHead(200, { 'Content-Type': 'application/json' });
+            response.end( JSON.stringify(messages) );
+
+        } else {
             file.serve(request, response);
+        }
     }).resume();
 });
 var scriptFilterExtension = {
@@ -26,10 +40,29 @@ var scriptFilterExtension = {
   }
 };
 
+var messageLogging = {
+    outgoing: function (message, callback) {
+        if (message.data && message.data.text) {
+            var room = message.channel.slice(message.channel.lastIndexOf('/') + 1);
+            var dataStoreId = 'messages.' + room;
+            var messagesToKeep = 30;
+            var currentMessages = dataStore.getItem(dataStoreId);
+            if (!currentMessages)
+                currentMessages = [];
+
+            currentMessages.push(message);  
+            dataStore.setItem(dataStoreId, currentMessages.slice(0,messagesToKeep));
+        }
+        
+        callback(message);
+    }
+};
+
 bayeux.attach(server);
 bayeux.addExtension(scriptFilterExtension);
 bayeux.addExtension(gifMeExtension);
 bayeux.addExtension(diceRollExtension("dice me"));
+bayeux.addExtension(messageLogging);
 var port = process.env.PORT || 8001;
 server.listen(port);
 console.log('listening on: ' + port);
