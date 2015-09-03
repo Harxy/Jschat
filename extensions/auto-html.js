9fda4e887035c2139cfe24ae5141e03e86fab10f
steve.brazier <steve.brazier@just-eat.com>
2015-09-03 14:01:11 +0100

    imagify any links to png gif or jpg

--- extensions/auto-html.js
+++ extensions/auto-html.js
@@ -1,6 +1,11 @@
-var linkRegex = /((https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?)/gi;
+var linkRegex = /[^'"]?((https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?)/gi;
+var imageLinkRegex = /(<a[^>]+>)(.+\.(png|gif|jpg))<\/a>/gi;
+
 var linkifyUrls = function(text) {
     return text.replace(linkRegex, "<a href='$1' target='_blank'>$1</a>");
+};
+var imagifyLinks = function(text) {
+    return text.replace(imageLinkRegex, "$1<img src='$2' /></a>");
 };
 
 var AutoHtmlExtension = {
@@ -10,6 +15,7 @@ var AutoHtmlExtension = {
             return;
         }
         message.data.text = linkifyUrls(message.data.text);
+        message.data.text = imagifyLinks(message.data.text);
         callback(message);
     }
 };
