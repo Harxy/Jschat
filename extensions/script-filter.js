72147cea4a1beb3fb5b08ec41349d6005b4b690f
steve.brazier <steve.brazier@just-eat.com>
2015-09-03 10:45:35 +0100

    put script filter into separate file

---
+++ extensions/script-filter.js
@@ -0,0 +1,38 @@
+String.prototype.filterOutScriptTags = function () {
+    var i = 0, scriptTagLevel = 0;
+    var result = "";
+    var str = this.toString();
+    while (i < str.length) {
+        // if we're at the start of a script tag, increase the scriptTagLevel
+        if (str.length - i >= 7 && str.substring(i, i + 7) == "\<script") {
+            scriptTagLevel++;
+            i = i + 7;
+            // if we're at the end of a script tag, decrease the scriptTagLevel (don't let it go below 0)
+        } else if (str.length - i >= 9 && str.substring(i, i + 9) == "\</script\>") {
+            if (scriptTagLevel > 0) {
+                scriptTagLevel--;
+            }
+
+            i = i + 9;
+            // else, use the scriptTagLevel to determine whether to include the text or not
+        } else {
+            if (scriptTagLevel == 0) {
+                result += str[i];
+            }
+
+            i++;
+        }
+    }
+
+    return result;
+};
+
+var ScriptFilterExtension = {
+    incoming: function (message, callback) {
+        if (message.data && message.data.text) {
+            message.data.text = message.data.text.filterOutScriptTags();
+        }
+        callback(message);
+    }
+};
+module.exports = ScriptFilterExtension;
