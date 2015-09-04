7ff196adcf6e40428dcd7450434a7b7b4f832535
Oli Farago <oli.farago@m7re.co.uk>
2015-09-04 21:03:07 +0100

    Pick random GIF fixes #36

--- extensions/gif-me.js
+++ extensions/gif-me.js
@@ -14,15 +14,17 @@ var GifMeExtension = {
             dontSend = true;
             var param = message.data.text.slice(gifMeKeyword.length).trim();
 
-            var url = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=1&q=' + param;
+            var url = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=25&q=' + param;
             request({
                 url: url,
                 json: true
             }, function(error, response, body) {
 
                 if (!error && response.statusCode === 200) {
-                    if (body.data.length > 0)
-                        message.data.text = '<img src="' + body.data[0].images.original.url + '"/>';
+                    if (body.data.length > 0) {
+                        var idToUse = Math.floor(Math.random() * (body.data.length));
+                        message.data.text = '<img src="' + body.data[idToUse].images.original.url + '"/>';
+                    }
                 }
 
                 callback(message);
