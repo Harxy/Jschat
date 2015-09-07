e9aefe84b8bdd6600e4007783ecd5dcbcda7e150
Paul Harker <harkerp@gmail.com>
2015-09-07 15:59:37 +0100

    corrected regex to not match hashtags in urls or next to other letters

--- extensions/auto-html.js
+++ extensions/auto-html.js
@@ -1,6 +1,6 @@
 var linkRegex = /((https?:\/\/)([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?)/gi;
 var imageLinkRegex = /(<a[^>]+>)([^<]+\.(png|gif|jpg))<\/a>/gi;
-var roomMeRegex = /#([A-Za-z0-9_-]+)(?![A-Za-z0-9_\]-])/g;
+var roomMeRegex = /( |^)#([A-Za-z0-9_-]+)(?![A-Za-z0-9_\]-])/g;
 
 var linkifyUrls = function(text) {
     return text.replace(linkRegex, "<a href='$1' target='_blank'>$1</a>");
@@ -9,7 +9,7 @@ var imagifyLinks = function(text) {
     return text.replace(imageLinkRegex, "$1<img src='$2' /></a>");
 };
 var roomifyLinks = function(text) {
-  return text.replace(roomMeRegex, "<a href='/rooms/$1'>#$1</a>");
+  return text.replace(roomMeRegex, "<a href='/rooms/$1'>#$2</a>");
 };
 
 var AutoHtmlExtension = {
