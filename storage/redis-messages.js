59b812304e2091b4d7008d7f4ef07b0364482989
steve.brazier <steve.brazier@just-eat.com>
2015-09-09 22:02:05 +0100

    make redis stored messages expire after 3 days

--- storage/redis-messages.js
+++ storage/redis-messages.js
@@ -1,10 +1,12 @@
-var CreateMessageStorage = function(redis, messagesToKeep){
+var CreateMessageStorage = function(redis, messagesToKeep) {
+    var roomLife = 3 * 60 * 60 * 24; // 3 days
     messagesToKeep = messagesToKeep || 30;
     var loadMessages = function (roomId, callback) {
         var dataStoreId = 'messages.' + roomId;
         redis.get(dataStoreId, function(err, reply) {
             callback(JSON.parse(reply) || []);
         });
+        redis.expire(dataStoreId, roomLife);
     };
     return {
         "storeMessage": function (roomId, message) {
