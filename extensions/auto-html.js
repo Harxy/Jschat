fd585ce52c19b13c327b6a019a410afec7995542
Paul Harker <harkerp@gmail.com>
2015-09-11 23:48:46 +0100

    removes roomception - sorry steve

--- extensions/auto-html.js
+++ extensions/auto-html.js
@@ -1,7 +1,6 @@
 var linkRegex = /( |^)((https?:\/\/)([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?)/gi;
 var imageLinkRegex = /(<a[^>]+>)([^<]+\.(png|gif|jpg))<\/a>/gi;
 var roomMeRegex = /( |^)#([A-Za-z0-9_-]+)(?![A-Za-z0-9_\]-])/g;
-var roomceptionRegex = /^roomcep(tion)? me #([A-Za-z0-9_-]+)(?![A-Za-z0-9_\]-])/gi;
 
 var linkifyUrls = function(text) {
     return text.replace(linkRegex, "$1<a href='$2' target='_blank'>$2</a>");
@@ -9,9 +8,7 @@ var linkifyUrls = function(text) {
 var imagifyLinks = function(text) {
     return text.replace(imageLinkRegex, "$1<img src='$2' /></a>");
 };
-var roomception = function(text) {
-    return text.replace(roomceptionRegex, '<iframe height="700" width="100%" src="/rooms/$2">');
-};
+
 var roomifyLinks = function(text) {
   return text.replace(roomMeRegex, "$1<a href='/rooms/$2'>#$2</a>");
 };
@@ -31,7 +28,6 @@ var AutoHtmlExtension = {
         message.data.text = apply(message.data.text,[
             linkifyUrls,
             imagifyLinks,
-            roomception,
             roomifyLinks
         ]);
         callback(message);
