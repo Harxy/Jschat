c7894a1a2cb2fcb430ea2440b82dfa53e3d75777
steve.brazier <steve.brazier@just-eat.com>
2015-09-11 14:38:52 +0100

    add some comments for storage

--- storage/loader.js
+++ storage/loader.js
@@ -1,9 +1,15 @@
 var loadDataStore = function(env) {
     var dataStore;
+
+    // REDIS is the preferred option. Heroku will have the REDIS_URL
+    // set if this is available.
     if (env.REDIS_URL) {
         var client = require('redis').createClient(env.REDIS_URL);
         var redisStorage = require('./redis-messages');
         dataStore = redisStorage.new(client, 30);
+
+    // If redis isn't available then node persist can store the messages
+    // on the local file system
     } else {
         var nodePersist = require('node-persist');
         var nodePersistStorage = require('./node-persist-messages');
