35d0ff02ca14fc40048f8be2ebb374f10264be5d
steve.brazier <steve.brazier@just-eat.com>
2015-09-16 09:36:44 +0100

    add emoji me command to list all the emojis available.

--- extensions/emojis.js
+++ extensions/emojis.js
@@ -4,7 +4,11 @@ var emoji = require("node-emoji");
 var EmojiExtension = {
     incoming: function (message, callback) {
         if (message.data && message.data.text) {
-            message.data.text = emoji.emojify(message.data.text);
+            if (message.data.text == "emoji me") {
+                message.data.text = Object.keys(emoji.emoji).join(", ");
+            } else {
+                message.data.text = emoji.emojify(message.data.text);
+            }
         }
         callback(message);
     }
