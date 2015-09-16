c48cd1da4c8e93c1b32d021648c7e3edd578adad
steve.brazier <steve.brazier@just-eat.com>
2015-09-16 09:40:04 +0100

    cache available emoji string

--- extensions/emojis.js
+++ extensions/emojis.js
@@ -1,11 +1,12 @@
 var emoji = require("node-emoji");
 
+var availableEmojiString = Object.keys(emoji.emoji).join(", ");
 
 var EmojiExtension = {
     incoming: function (message, callback) {
         if (message.data && message.data.text) {
             if (message.data.text == "emoji me") {
-                message.data.text = Object.keys(emoji.emoji).join(", ");
+                message.data.text = availableEmojiString;
             } else {
                 message.data.text = emoji.emojify(message.data.text);
             }
