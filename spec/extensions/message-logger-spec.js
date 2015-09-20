018c571ebacb3a8f83a4a93d54346e4474fd8f5f
steve.brazier <meadsteve@gmail.com>
2015-09-20 13:13:23 +0100

    add spec for the message logger

---
+++ spec/extensions/message-logger-spec.js
@@ -0,0 +1,51 @@
+var messageLogging = require("../../lib/extensions/message-logger");
+
+var messageWithText = function(text) {
+    return {
+        channel: "/rooms/logging-test",
+        data: {
+            text: text
+        }
+    };
+};
+
+var getMessageText = function(message) {
+    return message.data.text;
+};
+
+var expectMessageToMatch = function (updatedMessage, expectedMessage) {
+    expect(getMessageText(updatedMessage))
+        .toBe(getMessageText(expectedMessage));
+};
+
+describe("The message logger wraps data storage and provides and extension", function() {
+    var handler, dataStore;
+
+    beforeEach(function() {
+        dataStore = {
+            storeMessage: function (room, message){}
+        };
+        handler = messageLogging(dataStore).incoming;
+        spyOn(dataStore, 'storeMessage');
+    });
+
+
+    it("leaves messages unchanged", function() {
+        var message = messageWithText("I'm a little message and I'm okay");
+        var expectedMessage = messageWithText("I'm a little message and I'm okay");
+        handler(message, function(updatedMessage) {
+            expectMessageToMatch(updatedMessage, expectedMessage);
+        });
+    });
+
+    it("passes the room and message to the storage", function() {
+        var messageText = "I'm a little message and I'm okay";
+        var message = messageWithText(messageText);
+        handler(message, function(updatedMessage) {
+            expect(dataStore.storeMessage.calls.argsFor(0))
+                .toEqual(["logging-test", message]);
+        });
+    });
+
+
+});
