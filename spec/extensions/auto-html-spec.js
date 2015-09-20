1b2783b4950a073a26cdf7866f0bfb5899a8ee95
steve.brazier <meadsteve@gmail.com>
2015-09-20 12:28:00 +0100

    add first basic spec for the auto html extension

---
+++ spec/extensions/auto-html-spec.js
@@ -0,0 +1,68 @@
+autoHtmlExtension = require("../../lib/extensions/auto-html");
+
+var messageWithText = function(text) {
+    return {
+        data: {
+            text: text
+        }
+    };
+};
+
+var getMessageText = function(message) {
+    return message.data.text;
+};
+
+var expectMessageToMatch = function (updatedMessage, expectedMessage) {
+    expect(getMessageText(updatedMessage))
+        .toBe(getMessageText(expectedMessage));
+};
+
+describe("The auto html provides an incoming handler", function() {
+    var handler = autoHtmlExtension.incoming;
+
+    it("leaves basic messages unchanged", function() {
+        var message = messageWithText("I'm a little message and I'm okay");
+        var expectedMessage = messageWithText("I'm a little message and I'm okay");
+        handler(message, function(updatedMessage) {
+            expectMessageToMatch(updatedMessage, expectedMessage);
+        });
+    });
+
+    it("creates anchor tags out of links", function() {
+        var message = messageWithText("go to http://www.fun.com");
+        var expectedMessage = messageWithText(
+            "go to <a href='http://www.fun.com' target='_blank'>http://www.fun.com</a>"
+        );
+        handler(message, function(updatedMessage) {
+            expectMessageToMatch(updatedMessage, expectedMessage);
+        });
+    });
+
+    it("it leaves quoted links alone", function() {
+        var message = messageWithText("go to 'http://www.fun.com'");
+        var expectedMessage = messageWithText("go to 'http://www.fun.com'");
+        handler(message, function(updatedMessage) {
+            expectMessageToMatch(updatedMessage, expectedMessage);
+        });
+    });
+
+    it("makes .gif links images", function() {
+        var message = messageWithText("http://www.fun.com/lol.gif");
+        var expectedMessage = messageWithText(
+            "<a href='http://www.fun.com/lol.gif' target='_blank'><img src='http://www.fun.com/lol.gif' /></a>"
+        );
+        handler(message, function(updatedMessage) {
+            expectMessageToMatch(updatedMessage, expectedMessage);
+        });
+    });
+
+    it("makes links starting with a # into doors to other rooms", function() {
+        var message = messageWithText("#hypnotoad");
+        var expectedMessage = messageWithText(
+            "<a href='/rooms/hypnotoad'>#hypnotoad</a>"
+        );
+        handler(message, function(updatedMessage) {
+            expectMessageToMatch(updatedMessage, expectedMessage);
+        });
+    });
+});
