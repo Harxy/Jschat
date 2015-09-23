5f0878705ad4c447ec3148035c79b16e325bcbb8
steve.brazier <steve.brazier@just-eat.com>
2015-09-23 14:42:11 +0100

    add jasmine-phatomjs gulp runner for front end separation.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
-var jasmine = require('gulp-jasmine');
+var jasmine = require('gulp-jasmine-phantom');
 
 gulp.task('lint', function() {
     return gulp.src('./lib/**/*.js')
@@ -8,9 +8,14 @@ gulp.task('lint', function() {
         .pipe(jshint.reporter('default'));
 });
 
-gulp.task('jasmine', function () {
-    return gulp.src('./spec/**/*-spec.js')
+gulp.task('jasmine-backend', function () {
+    return gulp.src('./spec/backend/**/*-spec.js')
         .pipe(jasmine());
 });
 
-gulp.task('default', ['lint', 'jasmine']);
+gulp.task('jasmine-frontend', function () {
+    return gulp.src('./spec/frontend/**/*-spec.js')
+        .pipe(jasmine({integration: true}));
+});
+
+gulp.task('default', ['lint', 'jasmine-backend', 'jasmine-frontend']);
