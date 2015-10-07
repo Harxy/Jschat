927a521ac3bbb69d221c5509963e34238fac475b
Paul Harker <harkerp@gmail.com>
2015-10-07 11:08:03 +0100

    deleted service helper and added getService and getVideoId methods to queue-me

--- spec/backend/extensions/queue-me-spec.js
+++ spec/backend/extensions/queue-me-spec.js
@@ -1,11 +1,10 @@
 var queueMeExtension = require("../../../lib/extensions/queue-me.js");
-var messageHelper = require("../../helpers/message-helper.js");
-
+var messageHelper = require("../../helpers/message-helper.js"); 
 var expectMessageToMatch = messageHelper.expectMessageToMatch;
 var messageWithText = messageHelper.messageWithText;
 
 describe("The queue-me extension takes 'queue me link' and passes the link to the queue", function() {
-    var handler, playQueue;
+    var handler, request, playQueue;
 
     beforeEach(function() {
         playQueue = {
@@ -24,11 +23,35 @@ describe("The queue-me extension takes 'queue me link' and passes the link to th
         });
     });
 
-    it("queue me link adds link to the queue", function(done) {
+    it("and the queue should have been called with Friday", function(done) {
       var message = messageWithText("queue me www.youtube.com/watch?v=kfVsfOSbJY0");
       handler(message, function(updatedMessage) {
-        expect(playQueue.queueItem).toHaveBeenCalledWith('logging-test', { data: {'service': 'youtube', id: 'kfVsfOSbJY0', title: 'Friday - Rebecca Black - Official Music Video' }, duration: 228000, requester: 'Mctest'});
-        done();
+          expect(playQueue.queueItem).toHaveBeenCalledWith(
+                  'logging-test', 
+                  { data: 
+                      {   'service': 'youtube', 
+                          id: 'kfVsfOSbJY0', 
+                          title: 'Friday - Rebecca Black - Official Music Video' 
+                      }, 
+                      duration: 228000, requester: 'Mctest'
+                  });
+          done();
+      });
+    });
+
+    it("with a strange link", function(done) {
+      var message = messageWithText("queue me https://youtu.be/3wzqGI_4fIM");
+      handler(message, function(updatedMessage) {
+          expect(playQueue.queueItem).toHaveBeenCalledWith(
+                  'logging-test', 
+                  { data: 
+                      {   'service': 'youtube', 
+                          id: 'kfVsfOSbJY0', 
+                          title: 'Friday - Rebecca Black - Official Music Video' 
+                      }, 
+                      duration: 228000, requester: 'Mctest'
+                  });
+          done();
       });
     });
 });
