ca04f5890b14aa873e01fd8a4f992a3b0e3d3df7
steve.brazier <steve.brazier@just-eat.com>
2015-10-08 08:31:31 +0100

    make sure auto-html-spec uses async properly

--- spec/backend/extensions/auto-html-spec.js
+++ spec/backend/extensions/auto-html-spec.js
@@ -7,49 +7,54 @@ var messageWithText = messageHelper.messageWithText;
 describe("The auto html provides an incoming handler", function() {
     var handler = autoHtmlExtension.incoming;
 
-    it("leaves basic messages unchanged", function() {
+    it("leaves basic messages unchanged", function(done) {
         var message = messageWithText("I'm a little message and I'm okay");
         var expectedMessage = messageWithText("I'm a little message and I'm okay");
         handler(message, function(updatedMessage) {
             expectMessageToMatch(updatedMessage, expectedMessage);
+            done();
         });
     });
 
-    it("creates anchor tags out of links", function() {
+    it("creates anchor tags out of links", function(done) {
         var message = messageWithText("go to http://www.fun.com");
         var expectedMessage = messageWithText(
             "go to <a href='http://www.fun.com' target='_blank'>http://www.fun.com</a>"
         );
         handler(message, function(updatedMessage) {
             expectMessageToMatch(updatedMessage, expectedMessage);
+            done();
         });
     });
 
-    it("it leaves quoted links alone", function() {
+    it("it leaves quoted links alone", function(done) {
         var message = messageWithText("go to 'http://www.fun.com'");
         var expectedMessage = messageWithText("go to 'http://www.fun.com'");
         handler(message, function(updatedMessage) {
             expectMessageToMatch(updatedMessage, expectedMessage);
+            done();
         });
     });
 
-    it("makes .gif links images", function() {
+    it("makes .gif links images", function(done) {
         var message = messageWithText("http://www.fun.com/lol.gif");
         var expectedMessage = messageWithText(
             "<a href='http://www.fun.com/lol.gif' target='_blank'><img src='http://www.fun.com/lol.gif' /></a>"
         );
         handler(message, function(updatedMessage) {
             expectMessageToMatch(updatedMessage, expectedMessage);
+            done();
         });
     });
 
-    it("creates portals when room link is only content on line", function() {
+    it("creates portals when room link is only content on line", function(done) {
         var message = messageWithText("#hello");
         var expectedMessage = messageWithText(
             ":: portal opened to <a href='/rooms/hello'>#hello</a> (<a href='/rooms/hello' target='_blank'>in a new window</a>) ::"
         );
         handler(message, function(updatedMessage) {
             expectMessageToMatch(updatedMessage, expectedMessage);
+            done();
         });
     });
 
